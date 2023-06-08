2276dd5d34bd75b1bc5ac0a34ea4f67b4e0d2b97
Julia <juliaf92@mail.ru>
2023-06-08 21:15:00 +0300

    Added change

--- Sem5/Task4.py
+++ Sem5/Task4.py
@@ -8,6 +8,6 @@ from scipy import stats
 
 mothers = [172, 177, 158, 170, 178,175, 164, 160, 169, 165]
 daughters = [173, 175, 162, 174, 175, 168, 155, 170, 160, 163]
-print(stats.ttest_ind(mothers, daughters))
+print(stats.ttest_rel(mothers, daughters))
 print(f'pvalue > alfa, значит нулевая гипотеза не отвергается, '
       f'статистически значимых различий нет')
